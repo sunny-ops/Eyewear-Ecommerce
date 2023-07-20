5cd50449a42ae1f0b143f5fd1b273775e43e7a10
shizhong <shizhong1989@gmail.com>
2023-07-19 21:17:38 -0700

    Add Navbarshop.js

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import NavbarShop from "./components/NavbarShop.js";
+import Navbar from "./components/Navbar.js";
+import Banner from "./components/Banner.js";
+import Display from "./components/Display.js";
+import Footer from "./components/Footer.js";
+function Shop() {
+  return (
+    <>
+      <NavbarShop />
+      <Footer />
+    </>
+  );
+}
+
+export default Shop;
--- src/components/NavbarShop.js
+++ src/components/NavbarShop.js
@@ -3,7 +3,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./Navbar.css";
 
-function Navbar() {
+function NavbarShop() {
   return (
     <div className="navbar-container">
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
@@ -54,6 +54,7 @@ function Navbar() {
                 role="img"
                 aria-label="search"
                 className="anticon anticon-search searchbar-icon"
+                style={{ left: "10.6rem" }}
               >
                 <svg
                   viewBox="64 64 896 896"
@@ -63,15 +64,37 @@ function Navbar() {
                   height="1em"
                   fill="currentColor"
                   aria-hidden="true"
-                >
-                  <path d="M909.6 854.5L649.9 594.8C690.2 542.7 712 479 712 412c0-80.2-31.3-155.4-87.9-212.1-56.6-56.7-132-87.9-212.1-87.9s-155.5 31.3-212.1 87.9C143.2 256.5 112 331.8 112 412c0 80.1 31.3 155.5 87.9 212.1C256.5 680.8 331.8 712 412 712c67 0 130.6-21.8 182.7-62l259.7 259.6a8.2 8.2 0 0011.6 0l43.6-43.5a8.2 8.2 0 000-11.6zM570.4 570.4C528 612.7 471.8 636 412 636s-116-23.3-158.4-65.6C211.3 528 188 471.8 188 412s23.3-116.1 65.6-158.4C296 211.3 352.2 188 412 188s116.1 23.2 158.4 65.6S636 352.2 636 412s-23.3 116.1-65.6 158.4z"></path>
-                </svg>
+                ></svg>
               </span>
+              <div className="filters-toggle" role="presentation">
+                <button className="button-muted button-small" type="button">
+                  Filters &nbsp;
+                  <span
+                    role="img"
+                    aria-label="filter"
+                    className="anticon anticon-filter"
+                  >
+                    <svg
+                      viewBox="64 64 896 896"
+                      focusable="false"
+                      data-icon="filter"
+                      width="1em"
+                      height="1em"
+                      fill="currentColor"
+                      aria-hidden="true"
+                    >
+                      <path d="M880.1 154H143.9c-24.5 0-39.8 26.7-27.5 48L349 597.4V838c0 17.7 14.2 32 31.8 32h262.4c17.6 0 31.8-14.3 31.8-32V597.4L907.7 202c12.2-21.3-3.1-48-27.6-48zM603.4 798H420.6V642h182.9v156zm9.6-236.6l-9.5 16.6h-183l-9.5-16.6L212.7 226h598.6L613 561.4z"></path>
+                    </svg>
+                  </span>
+                </button>
+              </div>
+
               <input
                 className="search-input searchbar-input me-4"
                 type="search"
                 placeholder="Search Products..."
               />
+
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-baseline">
                 <li className="me-5">
                   <span
@@ -116,4 +139,4 @@ function Navbar() {
   );
 }
 
-export default Navbar;
+export default NavbarShop;
