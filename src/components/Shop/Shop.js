60dad43ba5aa267fcae08a83bfe5ea934fa829c0
shizhong <shizhong1989@gmail.com>
2023-07-24 22:39:46 -0700

    before add product component

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Products from "./Products";
-import Products1 from "./Products1";
 import Display from "./Products";
 
 function Shop() {
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Shop from "./Shop";
+// import Shop from "./Shop";
 import { BrowserRouter } from "react-router-dom";
 // import reportWebVitals from './reportWebVitals';
 
