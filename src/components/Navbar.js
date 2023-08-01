ab73e65aae04c0af839bf515a9709d7202c5cfe0
shizhong <shizhong1989@gmail.com>
2023-07-31 21:20:14 -0700

    Add cart badge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -393,7 +393,7 @@ function Navbar() {
 
               <ul className="navbar-nav me-auto mb-2 mb-lg-0 align-items-baseline">
                 <li className="me-5">
-                  <div>
+                  <div style={{ position: "relative" }}>
                     <span
                       role="img"
                       aria-label="shopping"
@@ -413,6 +413,12 @@ function Navbar() {
                         <path d="M832 312H696v-16c0-101.6-82.4-184-184-184s-184 82.4-184 184v16H192c-17.7 0-32 14.3-32 32v536c0 17.7 14.3 32 32 32h640c17.7 0 32-14.3 32-32V344c0-17.7-14.3-32-32-32zm-432-16c0-61.9 50.1-112 112-112s112 50.1 112 112v16H400v-16zm392 544H232V384h96v88c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-88h224v88c0 4.4 3.6 8 8 8h56c4.4 0 8-3.6 8-8v-88h96v456z"></path>
                       </svg>
                     </span>
+                    {cartState.length > 0 ? (
+                      <span class="badge-count">{cartState.length}</span>
+                    ) : (
+                      <></>
+                    )}
+
                     <Drawer
                       anchor="right"
                       open={state.right}
